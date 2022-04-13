fa5dcf6bc15d882979c73b89e9953b03758deee0
Jared.Tan <Jtanu45@gmail.com>
2022-04-13 13:34:57 -0700

    Added google login functionality, still needs to tie into database

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def init_app():
-   app = Flask(__name__)
+   app = Flask(__name__, static_url_path='')
 
    with app.app_context():
       # Import parts of our core Flask app
--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,25 @@
 """Application entry point."""
 from src import init_app
 import os
-'''
-CLIENT_ID = os.environ.get("CLIENT_ID")
-CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-AUTHORIZATION_BASE_URL =
-TOKEN_URL =
-USERINFO_URL =
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-# If we can't secure with SSL, we need to turn this on
-OS.ENVIRON["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-'''
+
+
 app = init_app()
+
+
+
+
+# Naive database setup
+'''
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
+
